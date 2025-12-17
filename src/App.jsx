f39739014c405286164ba7d72ea055edd1438904
Sakshi Shaw <shawsakshi100@gmail.com>
2025-12-17 14:02:23 +0530

    UI enhancement: lighter pastel theme and improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartScreen from "./pages/StartScreen";
 import GameScreen from "./pages/GameScreen";
+import "./index.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<StartScreen />} />
-        <Route path="/game" element={<GameScreen />} />
-      </Routes>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<StartScreen />} />
+          <Route path="/game" element={<GameScreen />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
